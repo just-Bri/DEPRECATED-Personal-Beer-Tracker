65ab244acd934c2cabfab3a31f1aea314454a401
bridgerrhammond@gmail.com <bridgerrhammond@gmail.com>
2016-06-08 13:21:35 -0500

    still trying to figure it out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,35 +21,33 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/*' do
-    binding.pry
-    @routes_all = []
-    self::Application.each_route.path_info {|r| @routes_all < r}
-    if @routes_all.include?(request.path_info)
+    ApplicationController::all_routes
+    if @@endpoints.include?(request.path_info)
       pass
     else
       erb :not_sure
     end
   end
 
-  def all_routes
-  endpoints= {}
+  def self.all_routes
+    @@endpoints= {}
     if Sinatra::Application.descendants.any?
       #Classic application structure
       applications = Sinatra::Application.descendants
       applications.each do |app|
-        endpoints[app.to_s.downcase.to_sym] = app.routes
+        @@endpoints[app.to_s.downcase.to_sym] = app.routes
       end
     elsif Sinatra::Base.descendants.any?
       #Modular application structure
       applications = Sinatra::Base.descendants
       applications.each do |app|
-        endpoints[app.to_s.downcase.to_sym] = app.routes
+        @@endpoints[app.to_s.downcase.to_sym] = app.routes
       end
     else
       abort("Cannot find any defined routes.....")
     end
 
-    endpoints.each do |app_name,routes|
+    @@endpoints.each do |app_name,routes|
       puts "Application: #{app_name}\n"
       routes.each do |verb,handlers|
         puts "\n#{verb}:\n"
