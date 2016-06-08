d9cdb26df35c436fe6b902fbfddd8710db22b7ba
bridgerrhammond@gmail.com <bridgerrhammond@gmail.com>
2016-06-08 15:00:28 -0500

    slight refactoring

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,6 +1,7 @@
 class BreweriesController < ApplicationController
   get '/breweries/:id' do #Load specific beer
     @brewery = Brewery.find_by_id(params[:id])
+    @beers = @brewery.beers
     erb :'breweries/show_brewery'
   end
 end
